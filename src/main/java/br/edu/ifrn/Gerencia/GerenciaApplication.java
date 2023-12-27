83ea159b908604c8085589ae59cf352ef9ea2cf0
Rafael-ASS <rarafaelalgusto@gmail.com>
2023-12-27 07:44:05 -0300

    Dependências

---
+++ src/main/java/br/edu/ifrn/Gerencia/GerenciaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifrn.Gerencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifrn/Gerencia/GerenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifrn.Gerencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
